199d424eae81296347e447f70e6f9116065833d0
Saurabh Yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-05-21 17:54:37 -0600

    update shop.php collection.php

--- collection.php
+++ collection.php
@@ -1,3 +1,38 @@
+<?php
+require_once('config/db.php');
+
+function e($value)
+{
+    return htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+}
+
+function category_image_path($path)
+{
+    if (empty($path)) {
+        return 'images/pista_demp.jpg';
+    }
+
+    if (preg_match('/^https?:\/\//i', $path)) {
+        return $path;
+    }
+
+    return 'admin/' . ltrim($path, '/');
+}
+
+$categories = [];
+$categoriesQuery = "
+    SELECT c.id, c.name, c.slug, c.image, COUNT(p.id) AS product_count
+    FROM categories c
+    LEFT JOIN products p ON p.category_id = c.id AND p.is_active = 1
+    WHERE c.is_active = 1
+    GROUP BY c.id, c.name, c.slug, c.image
+    ORDER BY c.name ASC
+";
+
+if ($result = $conn->query($categoriesQuery)) {
+    $categories = $result->fetch_all(MYSQLI_ASSOC);
+}
+?>
 <!doctype html>
 
 <html lang="en">
@@ -9,270 +44,81 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
   <meta name="description" content=""/>
 
-  <?php
-  include_once('includes/header-link.php');
-  ?>
+  <?php include_once('includes/header-link.php'); ?>
 </head>
 
 <body class="preload-wrapper popup-loader">
-  <!-- Scroll Top -->
-  <?php
-  include_once('includes/scroll-top.php');
-  ?>
-  <!-- /Scroll Top -->
-
-  <!-- preload -->
-  <?php
-  include_once('includes/preloader.php');
-  ?>
-  <!-- /preload -->
+  <?php include_once('includes/scroll-top.php'); ?>
+  <?php include_once('includes/preloader.php'); ?>
 
   <div id="wrapper">
-    <!-- Header -->
-    <?php
-    include_once('includes/header.php');
-    ?>
-    <!-- /Header -->
+    <?php include_once('includes/header.php'); ?>
 
-        <!-- page-title -->
-        <div class="page-title" style="background-image: url(images/breadcrumb_banner.jpg);">
-            <div class="container-full">
-                <div class="row">
-                    <div class="col-12">
-                        <h3 class="heading text-center">Our Collection</h3>
-                        <ul class="breadcrumbs d-flex align-items-center justify-content-center">
-                            <li>
-                                <a class="link" href="./">Home</a>
-                            </li>
-                            <li>
-                                <i class="icon-arrRight"></i>
-                            </li>
-                            <li>
-                                Our Collection
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+    <div class="page-title" style="background-image: url(images/breadcrumb_banner.jpg);">
+      <div class="container-full">
+        <div class="row">
+          <div class="col-12">
+            <h3 class="heading text-center">Our Collection</h3>
+            <ul class="breadcrumbs d-flex align-items-center justify-content-center">
+              <li><a class="link" href="./">Home</a></li>
+              <li><i class="icon-arrRight"></i></li>
+              <li>Our Collection</li>
+            </ul>
+          </div>
         </div>
-        <!-- /page-title -->
+      </div>
+    </div>
 
-        <!-- Collections -->
-        <section class="flat-spacing">
-            <div class="container">
-                <div class="tf-grid-layout tf-col-2 lg-col-4">
-                    <!-- item 1 -->
-                    <div class="collection-position-2 radius-lg style-3 hover-img">
-                        <a class="img-style">
-                            <img class="lazyload" data-src="https://aafarindryfruits.com/cdn/shop/collections/Untitled_design_27.png?v=1742812833"
-                                src="https://aafarindryfruits.com/cdn/shop/collections/Untitled_design_27.png?v=1742812833" alt="banner-cls">
-                        </a>
-                        <div class="content">
-                            <a href="shop.php" class="cls-btn">
-                                <h6 class="text">Pishtachio</h6> <span class="count-item text-secondary">28 items</span> <i
-                                    class="icon icon-arrowUpRight"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <!-- item 2 -->
-                    <div class="collection-position-2 radius-lg style-3 hover-img">
-                        <a class="img-style">
-                            <img class="lazyload" data-src="https://aafarindryfruits.com/cdn/shop/collections/Untitled_design_25.png?v=1742812776&width=535"
-                                src="https://aafarindryfruits.com/cdn/shop/collections/Untitled_design_25.png?v=1742812776&width=535" alt="banner-cls">
-                        </a>
-                        <div class="content">
-                            <a href="shop.php" class="cls-btn">
-                                <h6 class="text">Mamra Almond</h6> <span class="count-item text-secondary">18 items</span> <i
-                                    class="icon icon-arrowUpRight"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <!-- item 3 -->
-                    <div class="collection-position-2 radius-lg style-3 hover-img">
-                        <a class="img-style">
-                            <img class="lazyload" data-src="https://aafarindryfruits.com/cdn/shop/collections/Exploring_the_Healthiest_Dried_Fruits_for_Your_Dietary_Needs_1.jpg?v=1742823757"
-                                src="https://aafarindryfruits.com/cdn/shop/collections/Exploring_the_Healthiest_Dried_Fruits_for_Your_Dietary_Needs_1.jpg?v=1742823757" alt="banner-cls">
-                        </a>
-                        <div class="content">
-                            <a href="shop.php" class="cls-btn">
-                                <h6 class="text">Sundried Fruits</h6> <span class="count-item text-secondary">12 items</span> <i
-                                    class="icon icon-arrowUpRight"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <!-- item 4 -->
-                    <div class="collection-position-2 radius-lg style-3 hover-img">
-                        <a class="img-style">
-                            <img class="lazyload" data-src="https://aafarindryfruits.com/cdn/shop/collections/Untitled_design_26.png?v=1742813477"
-                                src="https://aafarindryfruits.com/cdn/shop/collections/Untitled_design_26.png?v=1742813477" alt="banner-cls">
-                        </a>
-                        <div class="content">
-                            <a href="shop.php" class="cls-btn">
-                                <h6 class="text">Cashew</h6> <span class="count-item text-secondary">15 items</span> <i
-                                    class="icon icon-arrowUpRight"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <!-- item 5 -->
-                    <div class="collection-position-2 radius-lg style-3 hover-img">
-                        <a class="img-style">
-                            <img class="lazyload" data-src="https://aafarindryfruits.com/cdn/shop/collections/Untitled_design_29.png?v=1742813069"
-                                src="https://aafarindryfruits.com/cdn/shop/collections/Untitled_design_29.png?v=1742813069" alt="banner-cls">
-                        </a>
-                        <div class="content">
-                            <a href="shop.php" class="cls-btn">
-                                <h6 class="text">Raisins</h6> <span class="count-item text-secondary">12 items</span> <i
-                                    class="icon icon-arrowUpRight"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <!-- item 6 -->
-                    <div class="collection-position-2 radius-lg style-3 hover-img">
-                        <a class="img-style">
-                            <img class="lazyload" data-src="https://aafarindryfruits.com/cdn/shop/collections/Untitled_design_28.png?v=1742812750"
-                                src="https://aafarindryfruits.com/cdn/shop/collections/Untitled_design_28.png?v=1742812750" alt="banner-cls">
-                        </a>
-                        <div class="content">
-                            <a href="shop.php" class="cls-btn">
-                                <h6 class="text">Walnuts</h6> <span class="count-item text-secondary">12 items</span> <i
-                                    class="icon icon-arrowUpRight"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <!-- item 7 -->
-                    <div class="collection-position-2 radius-lg style-3 hover-img">
-                        <a class="img-style">
-                            <img class="lazyload" data-src="https://aafarindryfruits.com/cdn/shop/collections/Leonardo_Phoenix_Ultrahigh_resolution_luxurious_gourmet_photog_3_1.jpg?v=1742815430"
-                                src="https://aafarindryfruits.com/cdn/shop/collections/Leonardo_Phoenix_Ultrahigh_resolution_luxurious_gourmet_photog_3_1.jpg?v=1742815430" alt="banner-cls">
-                        </a>
-                        <div class="content">
-                            <a href="shop.php" class="cls-btn">
-                                <h6 class="text">Dates</h6> <span class="count-item text-secondary">12 items</span> <i
-                                    class="icon icon-arrowUpRight"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <!-- item 8 -->
-                    <div class="collection-position-2 radius-lg style-3 hover-img">
-                        <a class="img-style">
-                            <img class="lazyload" data-src="https://aafarindryfruits.com/cdn/shop/collections/Exploring_the_Healthiest_Dried_Fruits_for_Your_Dietary_Needs.png?v=1742813692"
-                                src="https://aafarindryfruits.com/cdn/shop/collections/Exploring_the_Healthiest_Dried_Fruits_for_Your_Dietary_Needs.png?v=1742813692" alt="banner-cls">
-                        </a>
-                        <div class="content">
-                            <a href="shop.php" class="cls-btn">
-                                <h6 class="text">Anjeer</h6> <span class="count-item text-secondary">12 items</span>
-                                <i class="icon icon-arrowUpRight"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <!-- item 9 -->
-                    <div class="collection-position-2 radius-lg style-3 hover-img">
-                        <a class="img-style">
-                            <img class="lazyload" data-src="https://aafarindryfruits.com/cdn/shop/collections/Exploring_the_Healthiest_Dried_Fruits_for_Your_Dietary_Needs_2.jpg?v=1742814240"
-                                src="https://aafarindryfruits.com/cdn/shop/collections/Exploring_the_Healthiest_Dried_Fruits_for_Your_Dietary_Needs_2.jpg?v=1742814240" alt="banner-cls">
-                        </a>
-                        <div class="content">
-                            <a href="shop.php" class="cls-btn">
-                                <h6 class="text">Saffron</h6> <span class="count-item text-secondary">12 items</span> <i
-                                    class="icon icon-arrowUpRight"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <!-- item 10 -->
-                    <div class="collection-position-2 radius-lg style-3 hover-img">
-                        <a class="img-style">
-                            <img class="lazyload" data-src="https://aafarindryfruits.com/cdn/shop/collections/Leonardo_Phoenix_Ultrahigh_resolution_professional_food_photog_3.jpg?v=1742815076"
-                                src="https://aafarindryfruits.com/cdn/shop/collections/Leonardo_Phoenix_Ultrahigh_resolution_professional_food_photog_3.jpg?v=1742815076" alt="banner-cls">
-                        </a>
-                        <div class="content">
-                            <a href="shop.php" class="cls-btn">
-                                <h6 class="text">Seeds</h6> <span class="count-item text-secondary">12 items</span> <i
-                                    class="icon icon-arrowUpRight"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <!-- item 11 -->
-                    <div class="collection-position-2 radius-lg style-3 hover-img">
-                        <a class="img-style">
-                            <img class="lazyload" data-src="https://aafarindryfruits.com/cdn/shop/collections/green-tea-leaves.jpg?v=1742819201"
-                                src="https://aafarindryfruits.com/cdn/shop/collections/green-tea-leaves.jpg?v=1742819201" alt="banner-cls">
-                        </a>
-                        <div class="content">
-                            <a href="shop.php" class="cls-btn">
-                                <h6 class="text">Green Tea</h6> <span class="count-item text-secondary">12
-                                    items</span> <i class="icon icon-arrowUpRight"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <!-- item 12 -->
-                    <div class="collection-position-2 radius-lg style-3 hover-img">
-                        <a class="img-style">
-                            <img class="lazyload" data-src="https://aafarindryfruits.com/cdn/shop/collections/nh.jpg?v=1763106516"
-                                src="https://aafarindryfruits.com/cdn/shop/collections/nh.jpg?v=1763106516" alt="banner-cls">
-                        </a>
-                        <div class="content">
-                            <a href="shop.php" class="cls-btn">
-                                <h6 class="text">Honey</h6> <span class="count-item text-secondary">12 items</span>
-                                <i class="icon icon-arrowUpRight"></i>
-                            </a>
-                        </div>
-                    </div>
+    <section class="flat-spacing">
+      <div class="container">
+        <?php if (empty($categories)): ?>
+          <div class="text-center" style="padding: 50px 20px; background: #f8f8f8; border-radius: 8px;">
+            <h5>No collections found</h5>
+            <p class="text-secondary">Active categories added from the admin panel will appear here.</p>
+          </div>
+        <?php else: ?>
+          <div class="tf-grid-layout tf-col-2 lg-col-4">
+            <?php foreach ($categories as $category): ?>
+              <?php
+                $shopUrl = 'shop.php?category=' . urlencode($category['slug']);
+                $imagePath = category_image_path($category['image']);
+                $productCount = (int) $category['product_count'];
+              ?>
+              <div class="collection-position-2 radius-lg style-3 hover-img">
+                <a href="<?php echo e($shopUrl); ?>" class="img-style">
+                  <img
+                    class="lazyload"
+                    data-src="<?php echo e($imagePath); ?>"
+                    src="<?php echo e($imagePath); ?>"
+                    alt="<?php echo e($category['name']); ?>"
+                  />
+                </a>
+                <div class="content">
+                  <a href="<?php echo e($shopUrl); ?>" class="cls-btn">
+                    <h6 class="text"><?php echo e($category['name']); ?></h6>
+                    <span class="count-item text-secondary">
+                      <?php echo $productCount; ?> <?php echo $productCount === 1 ? 'item' : 'items'; ?>
+                    </span>
+                    <i class="icon icon-arrowUpRight"></i>
+                  </a>
                 </div>
-            </div>
-        </section>
-        <!-- /Collections -->
+              </div>
+            <?php endforeach; ?>
+          </div>
+        <?php endif; ?>
+      </div>
+    </section>
 
-        <!-- Footer -->
-    <?php
-    include_once('includes/footer.php');
-    ?>
-    <!-- /Footer -->
-
-    <!-- toolbar-bottom -->
-    <?php
-    include_once('includes/bottom-toolbar.php');
-    ?>
-    <!-- /toolbar-bottom -->
+    <?php include_once('includes/footer.php'); ?>
+    <?php include_once('includes/bottom-toolbar.php'); ?>
   </div>
 
-  <!-- auto popup  -->
-  <?php
-  include_once('includes/auto-popup.php');
-  ?>
-  <!-- /auto popup  -->
-
-  <!-- search -->
-  <?php
-  include_once('includes/search-bar.php');
-  ?>
-  <!-- /search -->
-
-  <!-- mobile menu -->
-  <?php
-  include_once('includes/mobile-menu.php');
-  ?>
-  <!-- /mobile menu -->
-
-  <!-- quickView -->
-    <?php
-    include_once('includes/quick-view.php');
-    ?>
-    <!-- quickView -->
-
-    <!-- Shopping Cart -->
-    <?php
-    include_once('includes/shopping-cart.php')
-    ?>
-    <!-- /Shopping Cart -->
-
-    <!-- wishlist -->
-    <?php
-    include_once('includes/wishlist-bar.php');
-    ?>
-    <!-- /wishlist -->
-
-  <?php
-  include_once('includes/footer-link.php');
-  ?>
+  <?php include_once('includes/auto-popup.php'); ?>
+  <?php include_once('includes/search-bar.php'); ?>
+  <?php include_once('includes/mobile-menu.php'); ?>
+  <?php include_once('includes/quick-view.php'); ?>
+  <?php include_once('includes/shopping-cart.php'); ?>
+  <?php include_once('includes/wishlist-bar.php'); ?>
+  <?php include_once('includes/footer-link.php'); ?>
 </body>
 </html>
